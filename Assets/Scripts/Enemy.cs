6fae5a815c9b927cd0e23d1b4509fd4a92ac48a9
Renan Vieira <rkvieira82@gmail.com>
2023-02-16 01:45:07 -0300

    Enemy spawn, basic movement, count, etc.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[SelectionBase]
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector3 targetPosition;
+    public float speed = 1f;
+
+    public int debugX = -100;
+    public int debugZ = -100;
+
+    private void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (transform.position == targetPosition)
+        {
+            GameManager.Instance.currentEnemyCount--;
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize(Vector3 targetPosition, int initialX, int initialZ)
     {
-        
+        this.targetPosition = targetPosition;
+        debugX = initialX;
+        debugZ = initialZ;
     }
 }
--- Assets/Scripts/WeaponCollider.cs
+++ Assets/Scripts/WeaponCollider.cs
@@ -22,6 +22,7 @@ public class WeaponCollider : MonoBehaviour
             {
                 print(3);
                 Destroy(other.gameObject);
+                GameManager.Instance.currentEnemyCount--;
             }
         }
     }
